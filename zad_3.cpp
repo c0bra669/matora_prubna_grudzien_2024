bc6a76c59a376673f8729692ca341e7db527a85e
c0bra <203257071+c0bra669@users.noreply.github.com>
2026-02-01 16:38:33 +0100

    zad 3.1

--- zad_3.cpp
+++ zad_3.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include<fstream>
 #include<vector>
+#include<cmath>
 void get_data(std::vector<int> &numbers,std::string path){
     std::ifstream file(path);
     int num{};
@@ -8,13 +9,32 @@ void get_data(std::vector<int> &numbers,std::string path){
         numbers.push_back(num);
     }
 }
+bool power_check(int number){
+    if(sqrt(number)==floor(sqrt(number))){
+        return true;
+    }
+    return false;
+}
 
+void print_nat_powers(std::vector<int> &numbers){
+    bool first_printed{false};
+    int count{};
+    for(int number : numbers){
+        if(power_check(number)){
+            if(!first_printed){
+                std::cout << number<<std::endl;
+                first_printed=true;
+            }
+            count++;
+        }
+    }
+    std::cout << count;
+}
 int main()
 {
     std::vector<int>numbers;
     get_data(numbers,"./files/liczby_przyklad.txt");
-    for(int number : numbers){
-        std::cout<<number<<std::endl;
-    }
+    print_nat_powers(numbers);
+
     return 0;
 }
